529126b188c1b5b96ff4e85341110e00cf186064
fpallares <fpallares@prensaiberica.es>
2024-02-20 14:21:54 +0100

    all excercise files without translation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import RegisterForm from './components/RegisterForm/RegisterForm';
+import React from 'react';
+import { IntlProvider } from 'react-intl';
+import Spanish from './lang/es.json';
+import French from './lang/fr.json';
+import English from './lang/en.json';
+
+const locale = navigator.language;
+let defaultMessages = Spanish;
+switch (locale) {
+  case 'fr-FR':
+    defaultMessages = French;
+    break;
+  case 'en-EN':
+    defaultMessages = English;
+    break;
+  case 'es-ES':
+    defaultMessages = Spanish;
+    break;
+  default:
+    defaultMessages = English;
+}
 
 function App() {
+  const [messages, setMessages] = React.useState(defaultMessages);
+
+  const handleLanguageChange = (selectedMessages) => {
+    setMessages(selectedMessages);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IntlProvider locale={locale} messages={messages}>
+      <div className='App'>
+        <RegisterForm />
+      </div>
+
+      <h2>Selector de idioma</h2>
+      <select onChange={(e) => handleLanguageChange(JSON.parse(e.target.value))}>
+        <option value={JSON.stringify(Spanish)}>Spanish</option>
+        <option value={JSON.stringify(English)}>English</option>
+        <option value={JSON.stringify(French)}>French</option>
+      </select>
+    </IntlProvider>
   );
 }
 
---
+++ src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,54 @@
+import { useForm } from 'react-hook-form';
+import React from 'react';
+import './RegisterForm.scss';
+import { FormattedMessage, useIntl } from 'react-intl';
+
+const RegisterForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const [formData, setFormData] = React.useState(null);
+
+  const onSubmit = (data) => setFormData(data);
+  const { formatMessage } = useIntl();
+
+  // Prueba de mensaje traducido en Javascript
+  console.log(formatMessage({ id: 'hello' }));
+
+  return (
+    <div className='register-form'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <fieldset className='register-form__fieldset'>
+          <input placeholder={formatMessage({ id: 'register_form:first_name' })} type='text' {...register('firstName', { required: true })} />
+          {errors?.firstName && (
+            <p className='register-form__error'>
+              <FormattedMessage id='register_form:required_field' />
+            </p>
+          )}
+        </fieldset>
+        <fieldset className='register-form__fieldset'>
+          <input placeholder={formatMessage({ id: 'register_form:last_name' })} type='text' {...register('lastName', { required: true, minLength: 3 })} />
+          {errors?.lastName && (
+            <p className='register-form__error'>
+              <FormattedMessage id='register_form:required_field_3_chars' />
+            </p>
+          )}
+        </fieldset>
+        <button type='submit'>Aceptar</button>
+      </form>
+
+      <h2>Datos del formulario:</h2>
+      <p>First name: {formData?.firstName}</p>
+      <p>Last name: {formData?.lastName}</p>
+
+      <h2>Datos del formulario en "caliente":</h2>
+      <p>First name: {watch('firstName')}</p>
+      <p>Last name: {watch('lastName')}</p>
+    </div>
+  );
+};
+
+export default RegisterForm;
